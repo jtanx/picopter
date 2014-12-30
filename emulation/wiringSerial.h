80c9d5cc4090799e77ca924acec4c91f03e4e71d
Jeremy Tan <jtanx@outlook.com>
2014-12-30 18:38:30 +0800

    Emulate some GPS data

--- emulation/wiringSerial.h
+++ emulation/wiringSerial.h
@@ -6,6 +6,14 @@
 
 #define serialOpen(a,b) 0
 #define serialClose(a)
-#define serialGetchar(a) 0
+static char serialGetchar(int fd) {
+    const char *t = "$GPGGA,123519,3157.132,S,11551.534,W,1,08,0.9,545.4,M,46.9,M,,*47";
+    static int p = 0;
+    if (!t[p]) {
+        p = 0;
+        return '\n';
+    }
+    return t[p++];
+}
 
 #endif //_WIRINGSERIAL_STUB_H
